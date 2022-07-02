330a7de199049fbf78611fcdcb987744c8f02652
omarafifii <48727083+omarafifii@users.noreply.github.com>
2022-07-03 00:04:35 +0200

    Add axios to fix jimp error

--- course-02/project/image-filter-starter-code/src/util/util.ts
+++ course-02/project/image-filter-starter-code/src/util/util.ts
@@ -1,5 +1,8 @@
+import { Axios, AxiosResponse } from "axios";
 import fs from "fs";
 import Jimp = require("jimp");
+const axios = require('axios').default;
+// require("axios")
 
 // filterImageFromURL
 // helper function to download, filter, and save the filtered image locally
@@ -11,16 +14,20 @@ import Jimp = require("jimp");
 export async function filterImageFromURL(inputURL: string): Promise<string> {
   return new Promise(async (resolve, reject) => {
     try {
-      const photo = await Jimp.read(inputURL);
-      const outpath =
-        "/tmp/filtered." + Math.floor(Math.random() * 2000) + ".jpg";
+      const image_response:AxiosResponse = await axios({
+        method: 'get',
+        url: inputURL,
+        responseType: 'arraybuffer'
+      })
+      const photo = await Jimp.read(image_response.data);
+      const outpath = "/tmp/filtered." + Math.floor(Math.random() * 2000) + ".jpg";
       await photo
-        .resize(256, 256) // resize
-        .quality(60) // set JPEG quality
-        .greyscale() // set greyscale
-        .write(__dirname + outpath, (img) => {
-          resolve(__dirname + outpath);
-        });
+      .resize(256, 256) // resize
+      .quality(60) // set JPEG quality
+      .greyscale() // set greyscale
+      .write(__dirname + outpath, (img) => {
+        resolve(__dirname + outpath);
+      });
     } catch (error) {
       reject(error);
     }

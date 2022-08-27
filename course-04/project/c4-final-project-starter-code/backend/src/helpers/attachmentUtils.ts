c2b7993706df534b8b6313f5da9af1587600024f
omarafifii <48727083+omarafifii@users.noreply.github.com>
2022-08-27 14:28:05 +0200

    Update attachmentUtils.ts

--- course-04/project/c4-final-project-starter-code/backend/src/helpers/attachmentUtils.ts
+++ course-04/project/c4-final-project-starter-code/backend/src/helpers/attachmentUtils.ts
@@ -1,6 +1,34 @@
+// TODO: Implement the fileStogare logic
 import * as AWS from 'aws-sdk'
 import * as AWSXRay from 'aws-xray-sdk'
+import { DocumentClient } from 'aws-sdk/clients/dynamodb'
+import { createLogger } from '../utils/logger'
+import { TodoItem } from '../models/TodoItem'
+import { TodoUpdate } from '../models/TodoUpdate';
+import { Types } from 'aws-sdk/clients/s3';
 
 const XAWS = AWSXRay.captureAWS(AWS)
 
-// TODO: Implement the fileStogare logic
+const logger = createLogger('TodosAccess')
+
+
+export class AttachmentUtils {
+    constructor(
+        private readonly s3Client: Types = new AWS.S3({ signatureVersion: 'v4' }),
+        private readonly s3BucketName = process.env.S3_BUCKET_NAME) {
+    }
+    
+
+    async generateUploadUrl(todoId: string): Promise<string> {
+        console.log("Generating URL");
+
+        const url = this.s3Client.getSignedUrl('putObject', {
+            Bucket: this.s3BucketName,
+            Key: todoId,
+            Expires: 1000,
+        });
+        console.log(url);
+
+        return url as string;
+    }
+}
